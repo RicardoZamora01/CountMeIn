416db7c6d03fa07730fb8c6bb5d8393f52e7f081
Ricardo Zamora <rzgarcia27@gmail.com>
2018-07-17 09:45:01 -0700

    Initial Commit

---
+++ CountMeIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountMeIn
+//
+//  Created by Ricardo Zamora on 7/17/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
