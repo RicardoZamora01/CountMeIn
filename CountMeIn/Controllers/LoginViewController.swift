9b805fc56b511d57527a3c9a3aa73e37497a427b
Ricardo Zamora <rzgarcia27@gmail.com>
2018-07-17 11:07:52 -0700

    added login view controller, connected it to login storyboard

---
+++ CountMeIn/Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  CountMeIn
+//
+//  Created by Ricardo Zamora on 7/17/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
