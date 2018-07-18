4a4a875268f56036d9c6b3a7da890ca3f5ad68f8
Ricardo Zamora <rzgarcia27@gmail.com>
2018-07-18 16:25:17 -0700

    added struct with info of organizations

---
+++ CountMeIn/Supporting/Organizations.swift
@@ -0,0 +1,70 @@
+//
+//  Organizations.swift
+//  CountMeIn
+//
+//  Created by Ricardo Zamora on 7/18/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+
+struct Organizations {
+    struct Animals {
+        struct AnimalCare{
+            static let name = "San Francisco Animal Care and Control"
+            static let address = "1200 15th St, San Francisco, CA 94103"
+            static let number = "(415) 554-6364"
+        }
+        
+        struct SFSPCA {
+            static let name = "SF SPCA"
+            static let address = "250 Florida St, San Francisco, CA 94103"
+            static let number = "(415) 522-3500"
+        }
+    }
+    
+    struct Beliefs {
+        struct LearningWorks {
+            static let name = "Jewish Learning Works"
+            static let address = "44 Page St Suite 604, San Francsico, CA 94102 "
+            static let number = "(415) 751-6983"
+            static let hours = "2 - 8 hours per event"
+        }
+        
+        struct TaoistReligiousFederation {
+            static let name = "United States Taoist Religious Federation"
+            static let address = "626 Grant Ave, San Francisco, CA 94108"
+            static let number = "(415) 362-2713"
+        }
+    }
+    
+    struct Education {
+        struct SFEducationFund {
+            static let name = "San Francisco Education Fund"
+            static let address = "2730 Bryant St, San Francisco, CA 94110"
+            static let number = "(415) 695-5400"
+            static let hours = "1 hour per week"
+        }
+        
+        struct TeachForAmerica {
+            static let name = "Teach For America"
+            static let address = "940 Howard St, San Francisco, CA 94103"
+            static let number = "(415) 659-0800"
+        }
+    }
+    
+    struct Poverty {
+        struct HomelessConnect {
+            static let name = "Project Homeless Connect"
+            static let address = "25 Van Ness Ave #340, San Francisco, CA, 94102"
+            static let number = "(415) 503-2123"
+        }
+        
+        struct OpenHand {
+            static let name = "Project Open Hand"
+            static let address = "730 Polk St, San Francisco, CA 94109"
+            static let number = "(415) 447-2300"
+            static let hours = "40 hours in 3 months"
+        }
+    }
+}
