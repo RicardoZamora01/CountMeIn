62f17dd16e721d138340bb84925830f69892ac16
Ricardo Zamora <rzgarcia27@gmail.com>
2018-07-19 11:32:04 -0700

    added selection view controller

---
+++ CountMeIn/Controllers/SelectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SelectionViewController.swift
+//  CountMeIn
+//
+//  Created by Ricardo Zamora on 7/19/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SelectionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- CountMeIn/Models/User.swift
+++ CountMeIn/Models/User.swift
@@ -7,52 +7,52 @@
 //
 
 import Foundation
-import FirebaseDatabase.FIRDataSnapshot
+//import FirebaseDatabase.FIRDataSnapshot
 
-class User: Codable {
-    // MARK: - Singleton
-    
-    // 1
-    private static var _current: User?
-    
-    // 2
-    static var current: User {
-        // 3
-        guard let currentUser = _current else {
-            fatalError("Error: current user doesn't exist")
-        }
-        
-        // 4
-        return currentUser
-    }
-    
-    // MARK: - Class Methods
-    
-    // 5
-    static func setCurrent(_ user: User, writeToUserDefaults: Bool = false) {
-        if writeToUserDefaults {
-            if let data = try? JSONEncoder().encode(user) {
-                UserDefaults.standard.set(data, forKey: Constants.UserDefaults.currentUser)
-            }
-        }
-        _current = user
-    }
-    
-    let uid: String
-    let username: String
-    
-    init(uid: String, username: String) {
-        self.uid = uid
-        self.username = username
-    }
-    
-    init?(snapshot: DataSnapshot) {
-        guard let dict = snapshot.value as? [String : Any],
-            let username = dict["username"] as? String
-            else { return nil }
-        
-        self.uid = snapshot.key
-        self.username = username
-    }
-    
-}
+//class User: Codable {
+//    // MARK: - Singleton
+//
+//    // 1
+//    private static var _current: User?
+//
+//    // 2
+//    static var current: User {
+//        // 3
+//        guard let currentUser = _current else {
+//            fatalError("Error: current user doesn't exist")
+//        }
+//
+//        // 4
+//        return currentUser
+//    }
+//
+//    // MARK: - Class Methods
+//
+//    // 5
+//    static func setCurrent(_ user: User, writeToUserDefaults: Bool = false) {
+//        if writeToUserDefaults {
+//            if let data = try? JSONEncoder().encode(user) {
+//                UserDefaults.standard.set(data, forKey: Constants.UserDefaults.currentUser)
+//            }
+//        }
+//        _current = user
+//    }
+//
+//    let uid: String
+//    let username: String
+//
+//    init(uid: String, username: String) {
+//        self.uid = uid
+//        self.username = username
+//    }
+//
+//    init?(snapshot: DataSnapshot) {
+//        guard let dict = snapshot.value as? [String : Any],
+//            let username = dict["username"] as? String
+//            else { return nil }
+//
+//        self.uid = snapshot.key
+//        self.username = username
+//    }
+
+//}
